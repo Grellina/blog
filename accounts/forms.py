349afc35b9b69da1a5f2a6ca3f60d7bc9e40dc3d
Zelenskaya Angelina <angel.zel@mail.ru>
2025-09-24 11:14:29 +0300

    modification of styles

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,18 +5,25 @@ from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput)
-    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput)
-
-
+    first_name = forms.CharField(max_length=100,
+                                 widget=forms.TextInput(attrs={'class': 'form-control mb-1',
+                                                               'placeholder': 'Enter First Name'}))
+    last_name = forms.CharField(max_length=100,
+                                widget=forms.TextInput(attrs={'class': 'form-control mb-1',
+                                                              'placeholder': 'Enter Last Name'}))
+    username = forms.CharField(max_length=100,
+                               widget=forms.TextInput(attrs={'class': 'form-control mb-1',
+                                                             'placeholder': 'Enter Username'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control mb-1',
+                                                           'placeholder': 'Enter your E-mail'}))
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control mb-1',
+                                                                                 'placeholder': 'Enter Password'}))
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={'class': 'form-control mb-1',
+                                                                                 'placeholder': 'Confirm Password'}))
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 
 class LoginForm(AuthenticationForm):
@@ -37,9 +44,11 @@ class LoginForm(AuthenticationForm):
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
                                required=True,
-                               widget=forms.TextInput())
+                               widget=forms.TextInput(attrs={'class': 'form-control mb-1',
+                                                             'placeholder': 'Username'}))
     email = forms.EmailField(required=True,
-                             widget=forms.TextInput())
+                             widget=forms.TextInput(attrs={'class': 'form-control mb-1',
+                                                             'placeholder': 'E-mail'}))
 
     class Meta:
         model = User
@@ -47,8 +56,8 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateProfileForm(forms.ModelForm):
-    avatar = forms.ImageField(widget=forms.FileInput())
-    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 5}))
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control mb-1'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Profile
