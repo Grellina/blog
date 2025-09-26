df8f01508bcc7b08a883973a4c66ea402f26b208
Zelenskaya Angelina <angel.zel@mail.ru>
2025-09-26 16:20:25 +0300

    rework API

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from blog.models import Post
+from .serializes import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserPostList(generics.ListAPIView):
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        user = self.kwargs['id']
+        return Post.objects.filter(author=user)
+
